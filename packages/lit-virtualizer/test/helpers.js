e1dc4fdd2b8697a72a601297af9d1f498dde1bb9
Brendan Baldwin <brendan@usergenic.com>
2022-05-02 12:39:46 -0600

    Add better comment to the wait() test helper.

--- packages/lit-virtualizer/test/helpers.js
+++ packages/lit-virtualizer/test/helpers.js
@@ -1,5 +1,13 @@
-// Ensure virtualizer does its thing by waiting for reflow and then use
-// setTimeout to defer by a microtask.
+/**
+ * @license
+ * Copyright 2021 Google LLC
+ * SPDX-License-Identifier: BSD-3-Clause
+ */
+
+/**
+ * Use this after rendering, resizing or scrolling to `await` the
+ * reflow necessary before validating events and visibility changes.
+ */
 export async function wait(n=0) {
     await new Promise(resolve => requestAnimationFrame(resolve));
     return new Promise(resolve => setTimeout(resolve, n));
